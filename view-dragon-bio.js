7f0622a3e30dfe9ea70fd49a77563ae88d01feff
corfidbizna <corfidbizna2@gmail.com>
2022-01-11 16:27:49 -0700

    Linewrap algorithm for Keyblade names in the bio.

--- view-dragon-bio.js
+++ view-dragon-bio.js
@@ -20,6 +20,23 @@ window.viewDragonBio = {
             url = path + this.keyblade.id + '.png';
             return (url || getDragonRenderURL(this.keyblade.dergID));
         },
+        processedTitle: function() {
+            var title = this.keyblade.name;
+            var lengthToWrap = 12;
+            if (title.length > lengthToWrap) {
+                var wrapAt;
+                var letters = title.split('');
+                for (let i = lengthToWrap; i > 0; i--) {
+                    if (letters[i] == ' ') {
+                        wrapAt = i;
+                        break;
+                    }
+                }
+                letters.splice(wrapAt, 1, '\n')
+                title = letters.join('');
+            }
+            return title;
+        },
     },
     template: /*html*/ `
         <div id="bio-box">
@@ -29,7 +46,9 @@ window.viewDragonBio = {
                 <img 
                     :src="framesPath + keyblade.element + '/bigs_L.png'"
                 >
-                <div class="key-name">{{this.keyblade.name}}</div>
+                <span 
+                    class="key-name"
+                >{{ processedTitle }}</span>
                 <img 
                     :src="
                         framesPath
@@ -38,24 +57,30 @@ window.viewDragonBio = {
                     "
                 >
             </div>
-            <img 
-                :src="framesPath + keyblade.element + '/top.png'"
-            >
+            <div>
+                <img 
+                    :src="framesPath + keyblade.element + '/top.png'"
+                >
+            </div>
             <div
                 class="horizontal-box"
             >
-                <img 
-                    :src="'https://corfid-portfolio.s3.amazonaws.com/flightrising_components/_DergImages/KingdomKey.png'"
-                    width="384"
-                    height="384"
+                <div 
+                    class="derg-art"
                 >
+                    <img 
+                        :src="'https://corfid-portfolio.s3.amazonaws.com/flightrising_components/_DergImages/KingdomKey.png'"
+                        width="384"
+                        height="384"
+                    >
+                </div>
                 <div
                     class="vertical-box"
                 >
                     <img 
                         :src="framesPath + keyblade.element + '/topShort.png'"
                     >
-                    <div
+                    <span
                         class="bio-text"
                     >
                         <br>
@@ -64,7 +89,7 @@ window.viewDragonBio = {
                         Kingdom Key isn't one to talk much, but he's always willing to help you out in a pinch. He's a notoriously subtle teacher—some dragons only realizing the lesson has taken place long after it happened. If you see Kingdom Key within earshot when such realizations are exclaimed aloud, you'll catch hints of a smirk on his face.
                         <br>
                         <br>
-                    </div>
+                    </span>
                     <img 
                         :src="framesPath + keyblade.element + '/bottomShort.png'"
                     >
