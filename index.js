52718d1b5951205ede23b04b1c112fd906cc96c4
corfidbizna <corfidbizna2@gmail.com>
2022-06-21 21:34:38 -0600

    Implemented using the left and right arrows to switch active Dragon.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var app = Vue.createApp({
             // activeKeyblade: 'KingdomKey',
         };
     },
+    mounted: function() {
+        window.document.body.addEventListener('keydown', this.keydownListener)
+    },
+    beforeDestroy: function() {
+        window.document.body.removeEventListener('keydown', this.keydownListener);
+    },
     computed: {
         getActiveDragon: function() {
             return this.keybladesMap[activeDragon];
@@ -20,6 +26,34 @@ var app = Vue.createApp({
     methods: {
         updateActiveDragon: function(newDragon) {
             this.activeDragon = newDragon;
+        },
+        keydownListener: function(keydownEvent) {
+            var direction = 0;
+            var keyblade = this.activeKeyblade;
+            var keybladeNames = Object.keys(keybladesMap);
+            if (keydownEvent.key === 'ArrowLeft') {
+                var direction = -1;
+                // keydownEvent.preventDefault(); 
+                // ^^ Hey Browser, don't do what you normally do. 
+            }
+            if (keydownEvent.key === 'ArrowRight') {
+                var direction = 1;
+            }
+            var activeBladeIndex = keybladeNames.findIndex((blade) => blade === keyblade);
+            var newActiveBladeIndex = activeBladeIndex + direction;
+            if (newActiveBladeIndex < 0) {
+                newActiveBladeIndex = keybladeNames.length - 1;
+            }
+            if (newActiveBladeIndex >= keybladeNames.length) {
+                newActiveBladeIndex = 0;
+            }
+            if (activeBladeIndex != newActiveBladeIndex) {
+                router.push(
+                    this.$route.fullPath.replace(
+                        keyblade, keybladeNames[newActiveBladeIndex]
+                    )
+                );
+            }
         },
     },
 });
